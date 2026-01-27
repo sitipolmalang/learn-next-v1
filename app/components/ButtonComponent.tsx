713aee73fa8187b3d16dad06b16d92578f9264af
Iqbal Digital <wahidikqbal@gmail.com>
2026-01-27 13:09:57 +0700

    first commit

---
+++ app/components/ButtonComponent.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface ButtonProps {
+    label: string;
+    onClick?: () => void;
+    variant?: 'primary' | 'secondary';
+}
+
+export default function ButtonComponent({
+    label, onClick, variant = 'primary',}: ButtonProps) {
+    const baseStyles = 'px-4 py-2 rounded font-semibold transition-colors rounded-lg ';
+    const variantStyles = {
+        primary: 'bg-blue-500 text-white hover:bg-blue-600',
+        secondary: 'bg-gray-300 text-black hover:bg-gray-400',
+    };
+
+    return (
+        <button
+            onClick={onClick}
+            className={`${baseStyles} ${variantStyles[variant]}`}
+        >
+            {label}
+        </button>
+    );
+}
